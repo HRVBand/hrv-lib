40709c212ea7aeb8cc2de6a26da93e1c31e5bbc8
Fenox <fenox@hotmail.de>
2017-02-18 21:43:04 +0100

    Added MathUtilTest

--- src/main/java/common/MathUtils.java
+++ src/main/java/common/MathUtils.java
@@ -16,23 +16,25 @@ public class MathUtils {
 	private MathUtils() {}
 	
 	/**
-	 * Rounds the given number (value) to places places.
+	 * Rounds the given number (value) to places places. With rounding mode half down (5.5 => 5)
 	 * @param value Value to round
 	 * @param places Number of places the given number should be rounded to
 	 * @return rounded number.
 	 */
 	public static double round(double value, int places) {
-	    if (places < 0) throw new IllegalArgumentException();
+	    if (places < 0) 
+	    	throw new IllegalArgumentException();
 
-	    BigDecimal bd = new BigDecimal(value);
+	    BigDecimal bd = BigDecimal.valueOf(value);
 	    bd = bd.setScale(places, RoundingMode.HALF_DOWN);
 	    return bd.doubleValue();
 	}
 	
 	public static double round(double value, int places, RoundingMode mode) {
-	    if (places < 0) { throw new IllegalArgumentException(); }
+	    if (places < 0) 
+	    { throw new IllegalArgumentException(); }
 
-	    BigDecimal bd = new BigDecimal(value);
+	    BigDecimal bd = BigDecimal.valueOf(value);
 	    bd = bd.setScale(places, mode);
 	    return bd.doubleValue();
 	}
--- src/test/java/common/ArrayUtilsTest.java
+++ src/test/java/common/ArrayUtilsTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 
-
 public class ArrayUtilsTest {
 	
 	 @Test 
