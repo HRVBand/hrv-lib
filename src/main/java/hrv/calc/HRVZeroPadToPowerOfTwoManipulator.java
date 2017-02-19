ad2c84aa8098c90d4ec7a339d3eb95741c8c032c
Fenox <fenox@hotmail.de>
2017-02-19 23:10:42 +0100

    Forgotten files...

---
+++ src/main/java/hrv/calc/HRVZeroPadToPowerOfTwoManipulator.java
@@ -0,0 +1,38 @@
+package hrv.calc;
+
+import common.ArrayUtils;
+import common.MathUtils;
+import hrv.RRData;
+import hrv.calc.frequency.AvgSampleSizeCalculator;
+
+/**
+ * Appends Zeores to given RRData until the size of RRData is a power of two.
+ * 
+ * @author Julian
+ *
+ */
+public class HRVZeroPadToPowerOfTwoManipulator implements HRVDataManipulator {
+
+	@Override
+	public void manipulate(RRData data) {
+		int length = data.getTimeAxis().length;
+
+		// Check if length is already a power of two.
+		if ((length & -length) == length) {
+			return; //If so return
+		}
+
+		int paddingUntil = MathUtils.largestNumThatIsPowerOf2(length) * 2;
+		int numOfNewNumbers = paddingUntil - length;
+
+		double[] newY = ArrayUtils.padZeros(data.getValueAxis(), numOfNewNumbers);
+
+		AvgSampleSizeCalculator calc = new AvgSampleSizeCalculator();
+		double avgSampleSize = calc.process(data);
+		double[] newX = ArrayUtils.continueWith(data.getTimeAxis(), avgSampleSize, numOfNewNumbers);
+
+		data.setTimeAxis(newX);
+		data.setValueAxis(newY);
+	}
+
+}
---
+++ src/test/java/hrv/calc/HRVZeroPaddingManipulatorTest.java
@@ -0,0 +1,46 @@
+package hrv.calc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import hrv.RRData;
+import units.TimeUnitConverter.TimeUnit;
+
+public class HRVZeroPaddingManipulatorTest {
+
+	@Test
+	public void testPadding() {
+		double[] time2 = new double[] { 0.0, 1.0, 2.0, 3.0, 4.0 };
+		double[] values2 = new double[] { 1.0, 1.1, 1.2, 0.9, 1.0 };
+		RRData data2 = new RRData(time2, TimeUnit.SECOND, values2, TimeUnit.SECOND);
+
+		HRVZeroPadToPowerOfTwoManipulator mani = new HRVZeroPadToPowerOfTwoManipulator();
+		mani.manipulate(data2);
+		
+		assertEquals(8, data2.getValueAxis().length);
+		assertEquals(1.0, data2.getValueAxis()[0], 0.00001);
+		assertEquals(1.0, data2.getValueAxis()[4], 0.00001);
+		assertEquals(0.0, data2.getValueAxis()[5], 0.00001);
+
+		assertEquals(8, data2.getTimeAxis().length);
+		assertEquals(0.0, data2.getTimeAxis()[0], 0.00001);
+		assertEquals(4.0, data2.getTimeAxis()[4], 0.00001);
+		assertEquals(5.0, data2.getTimeAxis()[5], 0.00001);
+		assertEquals(6.0, data2.getTimeAxis()[6], 0.00001);
+		assertEquals(7.0, data2.getTimeAxis()[7], 0.00001);
+	}
+	
+	@Test
+	public void testNoPadding() {
+		double[] time1 = new double[] { 0.0, 1.0, 2.0, 4.0 };
+		double[] values1 = new double[] { 1.0, 1.1, 1.2, 0.9 };
+		RRData data = new RRData(time1, TimeUnit.SECOND, values1, TimeUnit.SECOND);
+		
+		HRVZeroPadToPowerOfTwoManipulator mani = new HRVZeroPadToPowerOfTwoManipulator();
+		mani.manipulate(data);
+		
+		assertEquals(4, data.getTimeAxis().length);
+		assertEquals(4, data.getValueAxis().length);
+	}
+}
