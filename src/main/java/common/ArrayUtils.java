2dda966a51de00785c4463709340a0d9a186805b
Fenox <fenox@hotmail.de>
2017-02-19 16:51:04 +0100

    Added Methods and Tests to ArrayUtils

--- src/main/java/common/ArrayUtils.java
+++ src/main/java/common/ArrayUtils.java
@@ -1,6 +1,9 @@
 package common;
 
 import java.util.List;
+import java.util.concurrent.Callable;
+
+import hrv.RRData;
 
 /**
  * Utility class for array operations.
@@ -12,23 +15,26 @@ public class ArrayUtils {
 	/**
 	 * Makes this class a utility class.
 	 */
-	private ArrayUtils() {}
-	
+	private ArrayUtils() {
+	}
+
 	/**
 	 * Converts a List of type double to an array of type double
-	 * @param list List to convert
+	 * 
+	 * @param list
+	 *            List to convert
 	 * @return Array filled with the data of the given list.
 	 */
 	public static double[] listToArray(final List<Double> list) {
 		double[] array = new double[list.size()];
-				
-		for(int i = 0; i < list.size(); i++) {
+
+		for (int i = 0; i < list.size(); i++) {
 			array[i] = list.get(i);
 		}
-		
+
 		return array;
 	}
-	
+
 	public static double min(double[] array) {
 		double min = array[0];
 		for (int i = 1; i < array.length; i++) {
@@ -43,5 +49,53 @@ public class ArrayUtils {
 			max = Math.max(max, array[i]);
 		}
 		return max;
+	}
+
+	/**
+	 * Adds Zeros to the array until the index {@code untilIndex}.
+	 * 
+	 * @param data
+	 *            Data to be add zeroes to
+	 * @param untilIndex
+	 *            Number of zeroes to add. Must be positive
+	 * @return new array with trailing zeroes
+	 */
+	public static double[] padZeros(double[] data, int newZeroes) {
+		if (newZeroes < 0)
+			throw new IllegalArgumentException("Number of new zeroes must be larger than or equal to 0.");
+
+		double[] newData = new double[data.length + newZeroes];
+
+		// Transfer existing data
+		for (int i = 0; i < data.length; i++) {
+			newData[i] = data[i];
+		}
+
+		// Fill remaining values with zero and make x-Axis continuous
+		for (int i = data.length; i < data.length + newZeroes; i++) {
+			newData[i] = 0;
+		}
+
+		return newData;
+	}
+
+	public static double[] continueWith(double[] data, double increment, int numOfNewIncrements) {
+		if(numOfNewIncrements < 0)
+			throw new IllegalArgumentException("Number of new increments must be larger than or equal to 0.");
+				
+		double[] newData = new double[data.length + numOfNewIncrements];
+
+		// Transfer existing data
+		for (int i = 0; i < data.length; i++) {
+			newData[i] = data[i];
+		}
+
+		// Fill remaining values with zero and make x-Axis continuous
+		double firstOldElement = data.length == 0 ? 0 : data[data.length - 1];
+		for (int i = data.length; i < data.length + numOfNewIncrements; i++) {
+			newData[i] = firstOldElement + increment * i;
+		}
+
+		return newData;
 	}
 }
--- src/test/java/common/ArrayUtilsTest.java
+++ src/test/java/common/ArrayUtilsTest.java
@@ -6,30 +6,103 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 
 public class ArrayUtilsTest {
+
+	@Test
+	public void testListToArray() {
+		ArrayList<Double> list = new ArrayList<>();
+		list.add(1.0);
+		list.add(2.0);
+		list.add(3.0);
+		list.add(4.0);
+
+		double[] array = ArrayUtils.listToArray(list);
+
+		assertEquals(array.length, list.size());
+		assertEquals(array[0], list.get(0), 0.001);
+	}
+
+	@Test
+	public void testMin() {
+		double[] arr = new double[] { 1.0, 2.0, 0.0, 100.0, 99.9999, 0.000001 };
+		assertEquals(0.0, ArrayUtils.min(arr), 0.0000000001);
+	}
+
+	@Test
+	public void testMax() {
+		double[] arr = new double[] { 1.0, 2.0, 0.0, 100.0, 99.9999, 0.000001 };
+		assertEquals(100.0, ArrayUtils.max(arr), 0.0000000001);
+	}
+
+	@Test
+	public void testPadZeros() {
+		double[] arr = new double[] { 1.0, 2.0, 0.0, 100.0, 99.9999, 0.000001 };
+		double[] paddedArr = ArrayUtils.padZeros(arr, 5);
+		assertEquals(100.0, paddedArr[3], 0.0000000001);
+		assertEquals(0.000001, paddedArr[5], 0.0000000001);
+		assertEquals(0.0, paddedArr[6], 0.0000000001);
+		assertEquals(0.0, paddedArr[9], 0.0000000001);
+		assertEquals(10, paddedArr.length);
+	}
+
+	@Test
+	public void testPadNoZeroes() {
+		double[] arr = new double[] { 1.0, 2.0, 0.0, 100.0, 99.9999, 0.000001 };
+		double[] paddedArr = ArrayUtils.padZeros(arr, 0);
+
+		assertEquals(100.0, paddedArr[3], 0.0000000001);
+		assertEquals(0.000001, paddedArr[5], 0.0000000001);
+		assertEquals(arr.length, paddedArr.length);
+	}
+
+	@Test
+	public void testPadToEmptyArray() {
+		double[] arr = new double[0];
+
+		double[] paddedArr = ArrayUtils.padZeros(arr, 10);
+		assertEquals(10, paddedArr.length);
+		assertEquals(0.0, paddedArr[5], 0.0000000001);
+
+		double[] paddedArr2 = ArrayUtils.padZeros(arr, 0);
+		assertEquals(0, paddedArr2.length);
+	}
+
+	@Test
+	public void testContinouWith() {
+		double[] arr = new double[] { 0.0, 1.0, 2.0 };
+		double[] newArr = ArrayUtils.continueWith(arr, 1.0, 2);
+		
+		assertEquals(3.0, newArr[3], 0.00001);
+		assertEquals(4.0, newArr[4], 0.00001);
+		assertEquals(5, newArr.length);
+		
+
+		double[] arr2 = new double[] { 1.0, 2.0, 3.0 };
+		double[] newArr2 = ArrayUtils.continueWith(arr2, 1.0, 2);
+		
+		assertEquals(4.0, newArr2[3], 0.00001);
+		assertEquals(5.0, newArr2[4], 0.00001);
+		assertEquals(5, newArr2.length);
+	}
 	
-	 @Test 
-	 public void testListToArray() {
-		 ArrayList<Double> list = new ArrayList<>();
-		 list.add(1.0);
-		 list.add(2.0);
-		 list.add(3.0);
-		 list.add(4.0);
-		 
-		 double[] array = ArrayUtils.listToArray(list);
-		 
-		 assertEquals(array.length, list.size());
-		 assertEquals(array[0], list.get(0), 0.001);
-	 }
-	 
-	 @Test
-	 public void testMin() {
-		 double[] arr = new double[] {1.0, 2.0, 0.0, 100.0, 99.9999, 0.000001};
-		 assertEquals(0.0, ArrayUtils.min(arr), 0.0000000001);
-	 }
-	 
-	 @Test
-	 public void testMax() {
-		 double[] arr = new double[] {1.0, 2.0, 0.0, 100.0, 99.9999, 0.000001};
-		 assertEquals(100.0, ArrayUtils.max(arr), 0.0000000001);
-	 }
+	@Test
+	public void testContinouWithZeroLengthArray() {
+		double[] arr = new double[0];
+		double[] newArr = ArrayUtils.continueWith(arr, 2.0, 2);
+		
+		assertEquals(0.0, newArr[0], 0.00001);
+		assertEquals(2.0, newArr[1], 0.00001);
+		assertEquals(2, newArr.length);		
+	}
+	
+	@Test
+	public void testContinouWithDontContinou() {
+		double[] arr = new double[0];
+		double[] newArr = ArrayUtils.continueWith(arr, 1.0, 0);
+		assertEquals(0, newArr.length);		
+		
+
+		double[] arr2 = new double[] { 1.0, 2.0, 3.0 };
+		double[] newArr2 = ArrayUtils.continueWith(arr2, 1.0, 0);
+		assertEquals(3, newArr2.length);		
+	}
 }
