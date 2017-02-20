e13af9990cf1278203b1a94ac4424e8feac04973
Fenox <fenox@hotmail.de>
2017-02-20 17:08:17 +0100

    Made LFCalculator implement PowerSpectrumProcessor

--- src/main/java/hrv/calc/frequency/LFCalculator.java
+++ src/main/java/hrv/calc/frequency/LFCalculator.java
@@ -5,12 +5,12 @@ import org.apache.commons.math3.analysis.integration.TrapezoidIntegrator;
 import hrv.calc.frequency.psd.PowerSpectrum;
 import hrv.calc.frequency.psd.PowerSpectrumUnivariateFunctionAdapter;
 
-public class LFCalculator {
+public class LFCalculator implements HRVPowerSpectrumProcessor {
 
-	public double calculate(PowerSpectrum ps) {
+	@Override
+	public double process(PowerSpectrum ps) {
 		PowerSpectrumUnivariateFunctionAdapter psAdapter = new PowerSpectrumUnivariateFunctionAdapter(ps);
 		TrapezoidIntegrator integrator = new TrapezoidIntegrator();
-		double integral = integrator.integrate(TrapezoidIntegrator.DEFAULT_MAX_ITERATIONS_COUNT, psAdapter, 0.04, 0.15); 
-		return integral;
+		return integrator.integrate(TrapezoidIntegrator.DEFAULT_MAX_ITERATIONS_COUNT, psAdapter, 0.04, 0.15); 
 	}
 }
