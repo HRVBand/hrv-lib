d60e5da1e01121777db73d4cb3f12b309a304e7c
Fenox <fenox@hotmail.de>
2017-02-20 17:19:27 +0100

    Added test for LFCalculator

--- src/main/java/hrv/calc/frequency/LFCalculator.java
+++ src/main/java/hrv/calc/frequency/LFCalculator.java
@@ -7,10 +7,19 @@ import hrv.calc.frequency.psd.PowerSpectrumUnivariateFunctionAdapter;
 
 public class LFCalculator implements HRVPowerSpectrumProcessor {
 
+	private double lowerIntegrationLimit;
+	private double upperIntegrationLimit;
+
+	public LFCalculator(double lowerIntegrationLimit, double upperIntegrationLimit) {
+		this.lowerIntegrationLimit = lowerIntegrationLimit;
+		this.upperIntegrationLimit = upperIntegrationLimit;
+	}
+
 	@Override
 	public double process(PowerSpectrum ps) {
 		PowerSpectrumUnivariateFunctionAdapter psAdapter = new PowerSpectrumUnivariateFunctionAdapter(ps);
 		TrapezoidIntegrator integrator = new TrapezoidIntegrator();
-		return integrator.integrate(TrapezoidIntegrator.DEFAULT_MAX_ITERATIONS_COUNT, psAdapter, 0.04, 0.15); 
+		return integrator.integrate(TrapezoidIntegrator.DEFAULT_MAX_ITERATIONS_COUNT, psAdapter, lowerIntegrationLimit,
+				upperIntegrationLimit);
 	}
 }
---
+++ src/test/java/hrv/calc/frequency/LFCalculatorTest.java
@@ -0,0 +1,29 @@
+package hrv.calc.frequency;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import hrv.calc.frequency.psd.PowerSpectrum;
+
+public class LFCalculatorTest {
+
+	@Test
+	public void testOneFunction() {
+		int onesLength = 10;
+		double[] ones = new double[onesLength];
+		double[] onesX = new double[onesLength];
+		for(int i = 0; i < ones.length; i++) {
+			ones[i] = 1.0;
+			onesX[i] = i;
+		}
+		
+		PowerSpectrum ps = new PowerSpectrum(ones, onesX);
+		
+		LFCalculator calc = new LFCalculator(0, 5);
+		assertEquals(5, calc.process(ps), 0.01);
+
+		LFCalculator calc2 = new LFCalculator(0.04, 0.15);
+		assertEquals(0.11, calc2.process(ps), 0.0001);
+	}
+}
